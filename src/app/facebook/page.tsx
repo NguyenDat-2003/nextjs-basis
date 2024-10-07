b23f1332511f49edd15d0894144d866136ed9d05
Nguyen Tien Dat <maxdat2003@gmail.com>
2024-10-07 10:58:00 +0700

    Linking and Navigating

--- src/app/facebook/page.tsx
+++ src/app/facebook/page.tsx
@@ -1,7 +1,14 @@
+'use client'
+import { useRouter } from 'next/navigation'
 function Facebook() {
+  const router = useRouter()
+
   return (
     <>
       <p>facebook</p>
+      <button type='button' onClick={() => router.push('/')}>
+        Back
+      </button>
     </>
   )
 }
